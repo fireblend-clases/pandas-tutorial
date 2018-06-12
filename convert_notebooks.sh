6359a6198dad268df41176a0df5a37a48a0ad53d
Sergio Morales <sesquivel@growthaccelerationpartners.com>
2018-06-12 15:16:57 -0600

    Added solution-less notebooks

--- convert_notebooks.sh
+++ convert_notebooks.sh
@@ -1,1 +1,2 @@
-jupyter nbconvert --to=notebook --config nbconvert_config.py --output "01 - Introduccion.ipynb" "solved - 02 - Introduccion.ipynb"
+jupyter nbconvert --to=notebook --config nbconvert_config.py --output "01 - Introduccion.ipynb" "solved - 01 - Introduccion.ipynb"
+jupyter nbconvert --to=notebook --config nbconvert_config.py --output "06 - Modificacion de Datos (Reshaping).ipynb" "solved - 06 - Modificacion de Datos (Reshaping).ipynb"
